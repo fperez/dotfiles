e0c10cac1f03e0da0c1252034cb1ad45c155b5fd
Fernando Perez <fernando.perez@berkeley.edu>
2025-06-10 13:32:12 -0700

    Fix emacs colors for dark backgrounds

--- .emacs.conf.d/fonts-nw.el
+++ .emacs.conf.d/fonts-nw.el
@@ -5,17 +5,17 @@
 ;; own default is actually bold white.
 ;;(set-face-foreground 'default "White")
 
-(set-face-foreground 'font-lock-builtin-face "Blue")
-(set-face-foreground 'font-lock-comment-face "Red")
+(set-face-foreground 'font-lock-builtin-face "brightcyan")
+(set-face-foreground 'font-lock-comment-face "brightred")
 (set-face-foreground 'font-lock-doc-face "Green")
-(set-face-foreground 'font-lock-function-name-face "Yellow")
+(set-face-foreground 'font-lock-function-name-face "brightyellow")
 (set-face-foreground 'font-lock-keyword-face "Green")
 (set-face-foreground 'font-lock-other-type-face "Green")
 (set-face-foreground 'font-lock-preprocessor-face "Red")
-(set-face-foreground 'font-lock-reference-face "Yellow")
-(set-face-foreground 'font-lock-variable-name-face "Yellow")
-(set-face-foreground 'py-decorators-face "Yellow")
-(set-face-foreground 'comint-highlight-prompt "Yellow")
+(set-face-foreground 'font-lock-reference-face "brightyellow")
+(set-face-foreground 'font-lock-variable-name-face "brightyellow")
+(set-face-foreground 'py-decorators-face "brightyellow")
+(set-face-foreground 'comint-highlight-prompt "brightyellow")
 (set-face-foreground 'minibuffer-prompt "Blue")
 
 (set-face-background 'minibuffer-prompt "White")
@@ -25,6 +25,6 @@
 ;; Control bold attribute (on or off). It can help visibility in a terminal
 ;; with fewer colors available.
 (set-face-attribute 'font-lock-doc-face nil :bold nil)
-(set-face-attribute 'font-lock-constant-face nil :foreground "Yellow" :bold t)
+(set-face-attribute 'font-lock-constant-face nil :foreground "brightyellow" :bold t)
 (set-face-attribute 'font-lock-string-face nil :foreground "Blue" :bold t)
 (set-face-attribute 'font-lock-type-face nil :foreground "IndianRed4" :bold t)
